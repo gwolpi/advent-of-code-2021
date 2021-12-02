32a8c41e5c9ca0d7de23c7d5be566b87c715dc03
Gio Wolpert <giow@delta-n.nl>
2021-12-03 00:25:04 +0100

    ❄️ Day 02

--- days/02.ts
+++ days/02.ts
@@ -1,28 +1,24 @@
+const dirs: { [key: string]: { x?: 1 | -1, dy?: 1 | -1 }} = {
+  ['forward']: { x: 1 },
+  ['down']: { dy: 1 },
+  ['up']: { dy: -1 }
+};
+
 export const p1 = (input: string): number => {
-  const sub: { [key: string]: number } = { x: 0, y: 0 };
-  const dirs: { [key: string]: { x?: number, y?: number }} = {
-    ['forward']: { x: 1 },
-    ['down']: { y: 1 },
-    ['up']: { y: -1 }
-  };
+  const sub: { [key: string]: number } = { x: 0, dy: 0, y: 0 };
   [...input.matchAll(/(\w+)\s(\d+)/g)].forEach(([, action, amount]) => {
-    const subDir: { [key: string]: number } = dirs[action]
-    Object.keys(subDir).forEach((dir) => sub[dir] = Math.max(sub[dir] + subDir[dir] * +amount, 0));
+    const subDir: { [key: string]: 1 | -1 } = dirs[action]
+    Object.keys(subDir).forEach((dir) => sub[dir] += subDir[dir] * +amount);
   });
-  return sub.x * sub.y;
+  return sub.x * sub.dy;
 }
 
 export const p2 = (input: string): number => {
-  const sub: { [key: string]: number } = { x: 0, y: 0, a: 0 };
-  const dirs: { [key: string]: { x?: number, a?: number }} = {
-    ['forward']: { x: 1 },
-    ['down']: { a: 1 },
-    ['up']: { a: -1 }
-  };
+  const sub: { [key: string]: number } = { x: 0, dy: 0, y: 0 };
   [...input.matchAll(/(\w+)\s(\d+)/g)].forEach(([, action, amount]) => {  
-    const subDir: { [key: string]: number } = dirs[action];
-    Object.keys(subDir).forEach((dir) => sub[dir] = Math.max(sub[dir] + subDir[dir] * +amount, 0));
-    if (subDir.x) sub.y += +amount * sub.a;
+    const subDir: { [key: string]: 1 | -1 } = dirs[action];
+    Object.keys(subDir).forEach((dir) => sub[dir] += subDir[dir] * +amount);
+    if (subDir.x) sub.y += +amount * sub.dy;
   });
   return sub.x * sub.y;
 }
