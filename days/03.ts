ee1bb97ffbeaeef0c8417424ddbdcbc2c04f1d1b
Gio Wolpert <giow@delta-n.nl>
2021-12-03 12:32:53 +0100

    🎄 Day 03

--- days/03.ts
+++ days/03.ts
@@ -2,8 +2,8 @@ export const p1 = (input: string): number => {
   const lines = input.split('\n').map(line => line.split(''));
   const vertLines = lines[0].map((_, index) => lines.reduce((acc, line) => acc += line[index], ''));
   const binary = vertLines.map(x => {
-    const ohs = x.split('0');
-    return ohs.length - 1 >= ohs.join('').length ? '0' : '1';
+    const noughties = x.split('0');
+    return noughties.length - 1 >= noughties.join('').length ? '0' : '1';
   }).join('');
   const gammaRate  = parseInt(binary, 2);
   const epsilonRate = gammaRate ^ parseInt((new Array(binary.length+1)).join("1"),2);
@@ -13,13 +13,13 @@ export const p1 = (input: string): number => {
 export const p2 = (input: string): number => {
   const lines = input.split('\n');
   const [ oxygenGeneratorRating, co2ScrubberRating ] = ([
-    (ohs, ones) => `${Number(ohs <= ones)}`,
-    (ohs, ones) => `${Number(ohs > ones)}`, 
-  ] as ((ohs: number, linesLength: number) => string)[]).map((func) => {
+    (noughties, ones) => `${Number(noughties <= ones)}`,
+    (noughties, ones) => `${Number(noughties > ones)}`, 
+  ] as ((noughties: number, linesLength: number) => string)[]).map((func) => {
     let remainingLines = [ ...lines ];
     for(let i = 0; i < lines[0].length; i++) {
-      const remainingOhs = remainingLines.map(x => x[i]).join('').split('0').length - 1;
-      const bitForPos = func(remainingOhs, remainingLines.length - remainingOhs);
+      const remainingNoughties = remainingLines.map(x => x[i]).join('').split('0').length - 1;
+      const bitForPos = func(remainingNoughties, remainingLines.length - remainingNoughties);
       remainingLines = remainingLines.filter(x => x[i] === bitForPos);
       if(remainingLines.length === 1) return parseInt(remainingLines[0], 2);
     }
