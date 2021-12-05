2c513438d26d53d28fec3c0df1499e3727c66719
Gio Wolpert <giow@delta-n.nl>
2021-12-05 14:25:22 +0100

    🥵 Day 05

--- days/05.ts
+++ days/05.ts
@@ -4,25 +4,21 @@ const extractInput = (input: string) =>
   [...input.matchAll(/(\d+),(\d+) -> (\d+),(\d+)/g)]
     .map(x => x.map(Number))
     .map(([, x1, y1, x2, y2]) => ({ x1, y1, x2, y2}));
-const checkDiagram = (diagram: Diagram, x: number, y: number) => 
+const iterateDiagramCoordinate = (diagram: Diagram, x: number, y: number) => {
   ((diagram[y] ||= { [x]: 0 })[x] ||= 0);
+  diagram[y][x]++;
+}
 
 export const p1 = (input: string): number => {
   const diagram: Diagram = {};
   for(const { x1, y1, x2, y2 } of extractInput(input)) {
     if (x1 === x2) {
       const [start, end] = y1 > y2 ? [y2, y1] : [y1, y2];
-      for (let y = start; y <= end; y++) {
-        checkDiagram(diagram, x1, y);
-        ++diagram[y][x1];
-      }
+      for (let y = start; y <= end; y++) iterateDiagramCoordinate(diagram, x1, y);
     }
     if (y1 === y2) {
       const [start, end] = x1 > x2 ? [x2, x1] : [x1, x2];
-      for (let x = start; x <= end; x++) {
-        checkDiagram(diagram, x, y1);
-        ++diagram[y1][x];
-      }
+      for (let x = start; x <= end; x++) iterateDiagramCoordinate(diagram, x, y1);
     }
   }
   const cells = Object.values(diagram).flatMap(x => Object.values(x));
@@ -36,8 +32,7 @@ export const p2 = (input: string): number => {
     const dy = Math.sign(y2 - y1);
     for (let d = 0; d <= Math.abs(x1 === x2 ? y2 - y1 : x2 - x1); d++) {
       const [x, y] = [x1 + d * dx, y1 + d * dy];
-      checkDiagram(diagram, x, y);
-      ++diagram[y][x];
+      iterateDiagramCoordinate(diagram, x, y);
     }
   }
   const cells = Object.values(diagram).flatMap(x => Object.values(x));
