08cc8713ca67bbe33c599ac504316a82a2d53f1e
Gio Wolpert <giow@delta-n.nl>
2021-12-06 14:08:42 +0100

    🐠 Day 06

--- days/06.ts
+++ days/06.ts
@@ -1,13 +1,16 @@
-const RESET_TIMER_TO = 6;
-const BABY_FISH_TIMER = 8;
+const FISH_SPAWN_STAGE = 6;
+const FISH_LIFECYCLE = 8;
 
 const calculateFish = (input: string, days: number) => {
-  const fishPerLifeStage = Array.from({ length: BABY_FISH_TIMER + 1}, () => 0);
-  input.split(",").map(Number).forEach((lifeStage: number) => ++fishPerLifeStage[BABY_FISH_TIMER - lifeStage]);
+  const fishPerStage = input.split(",").reduce((acc: { [key: string]: number }, curr: string) => {
+    acc[curr] = (acc[curr] || 0 ) + 1;
+    return acc;
+  }, {} );
+  const fishPerLifeStage = Array.from({ length: FISH_LIFECYCLE + 1}, (_, i) => fishPerStage[i] || 0);
   for (let day = 0; day < days; day++) {
-    const createdFishes = fishPerLifeStage.pop();
-    fishPerLifeStage[BABY_FISH_TIMER - RESET_TIMER_TO - 1] += createdFishes!;
-    fishPerLifeStage.unshift(createdFishes!);
+    const createdFishes = fishPerLifeStage.shift();
+    fishPerLifeStage[FISH_SPAWN_STAGE] += createdFishes!;
+    fishPerLifeStage.push(createdFishes!);
   }
   return fishPerLifeStage.reduce((totalFish, fishInLifeStage) => totalFish + fishInLifeStage);
 }
