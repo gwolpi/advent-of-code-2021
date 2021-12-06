cd12c0ea5c13a298a9af95ad591825a86d20cabc
Gio Wolpert <giow@delta-n.nl>
2021-12-06 01:34:38 +0100

    🥵 Day 05

--- days/05.ts
+++ days/05.ts
@@ -1,40 +1,40 @@
-type Diagram = { [key: string]: { [key: string]: number }};
+type SetCollection = [Set<string>, Set<string>];
 
 const extractInput = (input: string) =>  
   [...input.matchAll(/(\d+),(\d+) -> (\d+),(\d+)/g)]
     .map(x => x.map(Number))
     .map(([, x1, y1, x2, y2]) => ({ x1, y1, x2, y2}));
-const iterateDiagramCoordinate = (diagram: Diagram, x: number, y: number) => {
-  ((diagram[y] ||= { [x]: 0 })[x] ||= 0);
-  diagram[y][x]++;
-}
+const iterateDiagramCoordinate = ([single, multiple]: SetCollection, x: number, y: number) => {
+  const xy = `${x**2 + 3*x + 2*x*y + y + y**2}`;
+  single.has(xy) ? multiple.add(xy) : single.add(xy);
+};
 
 export const p1 = (input: string): number => {
-  const diagram: Diagram = {};
+  const sets: SetCollection = [ new Set(), new Set() ];
   for(const { x1, y1, x2, y2 } of extractInput(input)) {
     if (x1 === x2) {
       const [start, end] = y1 > y2 ? [y2, y1] : [y1, y2];
-      for (let y = start; y <= end; y++) iterateDiagramCoordinate(diagram, x1, y);
+      for (let y = start; y <= end; y++) iterateDiagramCoordinate(sets, x1, y);
     }
     if (y1 === y2) {
       const [start, end] = x1 > x2 ? [x2, x1] : [x1, x2];
-      for (let x = start; x <= end; x++) iterateDiagramCoordinate(diagram, x, y1);
+      for (let x = start; x <= end; x++) iterateDiagramCoordinate(sets, x, y1);
     }
   }
-  const cells = Object.values(diagram).flatMap(x => Object.values(x));
-  return cells.filter(x => x >= 2).length;
+  const [, multiple] = sets;
+  return multiple.size;
 }
 
 export const p2 = (input: string): number => {
-  const diagram: Diagram = {};
+  const sets: SetCollection = [ new Set(), new Set() ];
   for (const {x1, y1, x2, y2} of extractInput(input)) {
     const dx = Math.sign(x2 - x1);
     const dy = Math.sign(y2 - y1);
     for (let d = 0; d <= Math.abs(x1 === x2 ? y2 - y1 : x2 - x1); d++) {
       const [x, y] = [x1 + d * dx, y1 + d * dy];
-      iterateDiagramCoordinate(diagram, x, y);
+      iterateDiagramCoordinate(sets, x, y);
     }
   }
-  const cells = Object.values(diagram).flatMap(x => Object.values(x));
-  return cells.filter(x => x >= 2).length;
+  const [, multiple] = sets;
+  return multiple.size;
 }
