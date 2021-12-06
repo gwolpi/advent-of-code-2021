643984c8fd919021d3cc447bd515d2fa2148bf7b
Gio Wolpert <giow@delta-n.nl>
2021-12-06 01:47:15 +0100

    🥵 Day 05

--- days/05.ts
+++ days/05.ts
@@ -1,24 +1,24 @@
-type SetCollection = [Set<string>, Set<string>];
+type CoordinateSets = [Set<string>, Set<string>];
 
 const extractInput = (input: string) =>  
   [...input.matchAll(/(\d+),(\d+) -> (\d+),(\d+)/g)]
     .map(x => x.map(Number))
     .map(([, x1, y1, x2, y2]) => ({ x1, y1, x2, y2}));
-const iterateDiagramCoordinate = ([single, multiple]: SetCollection, x: number, y: number) => {
-  const xy = `${x**2 + 3*x + 2*x*y + y + y**2}`;
+const assignCoordinateToSet = ([single, multiple]: CoordinateSets, x: number, y: number) => {
+  const xy = `x${x}y${y}`;
   single.has(xy) ? multiple.add(xy) : single.add(xy);
 };
 
 export const p1 = (input: string): number => {
-  const sets: SetCollection = [ new Set(), new Set() ];
+  const sets: CoordinateSets = [ new Set(), new Set() ];
   for(const { x1, y1, x2, y2 } of extractInput(input)) {
     if (x1 === x2) {
       const [start, end] = y1 > y2 ? [y2, y1] : [y1, y2];
-      for (let y = start; y <= end; y++) iterateDiagramCoordinate(sets, x1, y);
+      for (let y = start; y <= end; y++) assignCoordinateToSet(sets, x1, y);
     }
     if (y1 === y2) {
       const [start, end] = x1 > x2 ? [x2, x1] : [x1, x2];
-      for (let x = start; x <= end; x++) iterateDiagramCoordinate(sets, x, y1);
+      for (let x = start; x <= end; x++) assignCoordinateToSet(sets, x, y1);
     }
   }
   const [, multiple] = sets;
@@ -26,13 +26,13 @@ export const p1 = (input: string): number => {
 }
 
 export const p2 = (input: string): number => {
-  const sets: SetCollection = [ new Set(), new Set() ];
+  const sets: CoordinateSets = [ new Set(), new Set() ];
   for (const {x1, y1, x2, y2} of extractInput(input)) {
     const dx = Math.sign(x2 - x1);
     const dy = Math.sign(y2 - y1);
     for (let d = 0; d <= Math.abs(x1 === x2 ? y2 - y1 : x2 - x1); d++) {
       const [x, y] = [x1 + d * dx, y1 + d * dy];
-      iterateDiagramCoordinate(sets, x, y);
+      assignCoordinateToSet(sets, x, y);
     }
   }
   const [, multiple] = sets;
