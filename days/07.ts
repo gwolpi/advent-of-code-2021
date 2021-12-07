7cd427c27e5f8e88601fff6f172dee5eb7711402
Gio Wolpert <giow@delta-n.nl>
2021-12-07 08:33:00 +0100

    🦀 Day 07

--- days/07.ts
+++ days/07.ts
@@ -1,18 +1,16 @@
 export const p1 = (input: string) => {
   const positions = input.split(",").map(Number);
-  const fuelRequired = positions.reduce((x, _, i) => 
-    [ ...x, positions
-      .map((crab) => Math.abs(crab - i))
-      .reduce((a, b) => a + b)], [] as number[]);
-  return Math.min(...fuelRequired);
+  return Math.min(...positions.reduce((x, _, i) => 
+  [ ...x, positions
+    .map(crab => Math.abs(crab - i))
+    .reduce((a, b) => a + b)], [] as number[]));
 }
 
 export const p2 = (input: string): number => {
   const positions = input.split(",").map(Number);
-  const fuelRequired = positions.reduce((x, _, i) => 
-    [ ...x, positions
-      .map((crab) => Math.abs(crab - i))
-      .map(usage => usage * (usage + 1) / 2)
-      .reduce((a, b) => a + b)], [] as number[]);
-  return Math.min(...fuelRequired);
+  return Math.min(...positions.reduce((x, _, i) => 
+  [ ...x, positions
+    .map(crab => Math.abs(crab - i))
+    .map(usage => usage * (usage + 1) / 2)
+    .reduce((a, b) => a + b)], [] as number[]));
 }
