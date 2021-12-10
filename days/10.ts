6421999e9aa5255153d78416904d8f16e20a9880
Gio Wolpert <giow@delta-n.nl>
2021-12-10 18:15:48 +0100

    🧭 Day 10

--- days/10.ts
+++ days/10.ts
@@ -1,53 +1,38 @@
-const matches: { [key: string]: string } = {
-  ['(']: ')',
-  ['[']: ']',
-  ['{']: '}',
-  ['<']: '>'
+export const p1 = (input: string): number | undefined => {
+  const points: { [key: string]: number } = {
+    ')': 3, ']': 57, '}': 1197, '>': 25137
+  };
+  return input.split('\n').reduce((total, line) => {
+    while(true){
+      const oldLength = line.length + 0;
+      line = line.replaceAll(/\(\)|\[\]|\{\}|\<\>/g, '');
+      if (oldLength === line.length) {
+        const match = line.match(/(\)|\]|\}|\>)/);
+        if (match) total += points[match[1]];
+        return total;
+      } 
+    }
+  }, 0) 
 }
 
-export const p1 = (input: string): number => {
+export const p2 = (input: string): number | undefined => {
   const points: { [key: string]: number } = {
-    [')']: 3,
-    [']']: 57,
-    ['}']: 1197,
-    ['>']: 25137
-  }
-  return input.split('\n').reduce((acc, curr) => {
-    const expectedChars: string[] = [];
-    for (const char of curr.trim()) {
-      if (!Object.keys(matches).includes(char)) {
-        if ((char === expectedChars.pop())) continue;
-        acc += points[char];
-        break;
+    '(': 1, '[': 2, '{': 3, '<': 4
+  };
+  const result = input.split('\n').reduce((total, line) => {
+    while(true){
+      const oldLength = line.length + 0;
+      line = line.replaceAll(/\(\)|\[\]|\{\}|\<\>/g, '').trim();
+      if (oldLength === line.length) {
+        if (line.match(/(\)|\]|\}|\>)/)) return total;
+        return [
+          ...total,  
+          line.split('').reduceRight((acc, char) => 
+            (acc * 5) + points[char], 0)
+        ];
       } 
-      expectedChars.push(matches[char]);
     }
-    return acc;
-  },0)
-}
-
-export const p2 = (input: string): number => {
-  const points: { [key: string]: number } = {
-    [')']: 1,
-    [']']: 2,
-    ['}']: 3,
-    ['>']: 4
-  }
-  const pointsPerLine = input.split('\n').map(x => {
-    const expectedChars: string[] = [];
-    for (const char of x.trim()) {
-      if (!Object.keys(matches).includes(char)) {  
-        if ((char !== expectedChars.pop())) return null;
-        continue;
-      } 
-      expectedChars.push(matches[char]);
-    }
-    return expectedChars.reduceRight((acc, char) => 
-      (acc * 5) + points[char], 0);
-  });
-  const uppl = pointsPerLine
-    .filter(x => x !== null)
-    .map(Number)
+  }, [] as number[]) 
     .sort((a, b) => a - b);
-  return uppl[uppl.length / 2 | 0];
+  return result[result.length / 2 | 0];
 }
