89862810e75bc4166cdd9fce42ba7f2665ccd127
Gio Wolpert <giow@delta-n.nl>
2021-12-11 02:09:04 +0100

    🧭 Day 10

--- days/10.ts
+++ days/10.ts
@@ -1,9 +1,8 @@
+type PointsMap = { [key: string]: number };
 const regex = /\(\)|\[\]|\{\}|\<\>/g;
 
 export const p1 = (input: string): number | undefined => {
-  const points: { [key: string]: number } = {
-    ')': 3, ']': 57, '}': 1197, '>': 25137
-  };
+  const points: PointsMap = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
   return input.split('\n').reduce((total, line) => {
     while(line.length !== (line = line.replaceAll(regex, '').trim()).length);
     const match = line.match(/([\)\]\}\>])/);
@@ -12,9 +11,7 @@ export const p1 = (input: string): number | undefined => {
 }
 
 export const p2 = (input: string): number | undefined => {
-  const points: { [key: string]: number } = {
-    '(': 1, '[': 2, '{': 3, '<': 4
-  };
+  const points: PointsMap = { '(': 1, '[': 2, '{': 3, '<': 4 };
   const result = input.split('\n').reduce((total, line) => {
     while(line.length !== (line = line.replaceAll(regex, '').trim()).length);
     if (line.match(/([\)\]\}\>])/)) return total;
