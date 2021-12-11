16e7411b42a9e60905b3343c9ec0715d9777a515
Gio Wolpert <giow@delta-n.nl>
2021-12-11 02:45:09 +0100

    🧭 Day 10

--- days/10.ts
+++ days/10.ts
@@ -5,7 +5,7 @@ const corruptedLine = /([\)\]\}\>])/;
 export const p1 = (input: string): number | undefined => {
   const points: PointsMap = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
   return input.split('\n').reduce((score, line) => {
-    while(line.length !== (line = line.replaceAll(matchingBrackets, '').trim()).length);
+    while(line.length !== (line = line.replaceAll(matchingBrackets, '')).length);
     const isCorrupted = line.match(corruptedLine);
     return score += isCorrupted ? points[isCorrupted.pop()!] : 0;
   }, 0) 
@@ -14,7 +14,7 @@ export const p1 = (input: string): number | undefined => {
 export const p2 = (input: string): number | undefined => {
   const points: PointsMap = { '(': 1, '[': 2, '{': 3, '<': 4 };
   const result = input.split('\n').reduce((total, line) => {
-    while(line.length !== (line = line.replaceAll(matchingBrackets, '').trim()).length);
+    while(line.length !== (line = line.replaceAll(matchingBrackets, '')).length);
     if (!line.match(corruptedLine)) 
       total.push(line.split('').reduceRight((acc, char) => (acc * 5) + points[char], 0));
     return total;
