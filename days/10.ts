7b5383a13eb487351e60209924a302633efb964a
Gio Wolpert <giow@delta-n.nl>
2021-12-11 02:44:21 +0100

    🧭 Day 10

--- days/10.ts
+++ days/10.ts
@@ -1,24 +1,23 @@
 type PointsMap = { [key: string]: number };
-const regex = /\(\)|\[\]|\{\}|\<\>/g;
+const matchingBrackets = /\(\)|\[\]|\{\}|\<\>/g;
+const corruptedLine = /([\)\]\}\>])/;
 
 export const p1 = (input: string): number | undefined => {
   const points: PointsMap = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
-  return input.split('\n').reduce((total, line) => {
-    while(line.length !== (line = line.replaceAll(regex, '').trim()).length);
-    const match = line.match(/([\)\]\}\>])/);
-    return total += match ? points[match.pop()!] : 0;
+  return input.split('\n').reduce((score, line) => {
+    while(line.length !== (line = line.replaceAll(matchingBrackets, '').trim()).length);
+    const isCorrupted = line.match(corruptedLine);
+    return score += isCorrupted ? points[isCorrupted.pop()!] : 0;
   }, 0) 
 }
 
 export const p2 = (input: string): number | undefined => {
   const points: PointsMap = { '(': 1, '[': 2, '{': 3, '<': 4 };
   const result = input.split('\n').reduce((total, line) => {
-    while(line.length !== (line = line.replaceAll(regex, '').trim()).length);
-    if (line.match(/([\)\]\}\>])/)) return total;
-    total.push(line.split('').reduceRight((acc, char) => 
-      (acc * 5) + points[char], 0));
+    while(line.length !== (line = line.replaceAll(matchingBrackets, '').trim()).length);
+    if (!line.match(corruptedLine)) 
+      total.push(line.split('').reduceRight((acc, char) => (acc * 5) + points[char], 0));
     return total;
-  }, [] as number[]) 
-    .sort((a, b) => a - b);
-  return result[result.length / 2 | 0];
+  }, [] as number[]);
+  return result.sort((a, b) => a - b)[result.length / 2 | 0];
 }
