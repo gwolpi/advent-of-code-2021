c59a60d64babd3488ab4a5238e5e3612abf19866
Gio <MvOc0yRHIof9wo8OF@gio.dev>
2021-12-14 00:30:29 +0100

    ⛰️ Day 12

--- days/12.test.ts
+++ days/12.test.ts
@@ -1,16 +1,16 @@
 import { p1, p2 } from './12.ts';
 import { assertEquals } from "https://deno.land/x/std@0.65.0/testing/asserts.ts";
 
-const input = 'start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end';
+const input = 'dc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc';
 
 Deno.test('it should run the first part of day 12 correctly', () => {
   const result = p1(input);
-  const expected = 10;
+  const expected = 19;
   assertEquals(result, expected);
 });
 
 Deno.test('it should run the second part of day 12 correctly', () => {
   const result = p2(input);
-  const expected = 36;
+  const expected = 103;
   assertEquals(result, expected);
 });
