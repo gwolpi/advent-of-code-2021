9a32e9c031d2f9bac6438f6afbc36b3237c5759b
Gio <MvOc0yRHIof9wo8OF@gio.dev>
2021-12-21 17:06:28 +0100

    🎲 Day 21

--- days/21.test.ts
+++ days/21.test.ts
@@ -1,16 +1,16 @@
 import { p1, p2 } from './21.ts';
 import { assertEquals } from "https://deno.land/x/std@0.65.0/testing/asserts.ts";
 
-const input = '';
+const input = 'Player 1 starting position: 4\nPlayer 2 starting position: 8';
 
 Deno.test('it should run the first part of day 21 correctly', () => {
   const result = p1(input);
-  const expected = 0;
+  const expected = 739785;
   assertEquals(result, expected);
 });
 
 Deno.test('it should run the second part of day 21 correctly', () => {
   const result = p2(input);
-  const expected = 0;
+  const expected = 444356092776315;
   assertEquals(result, expected);
 });
